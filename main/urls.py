ad2a1978c277518a159f2b65624d742d758f2314
Nikita(PC) <nikita.verba04@mail.ru>
2021-10-06 16:18:33 +0300

    Added logout page

--- main/urls.py
+++ main/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('register/', register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='main/logout.html'), name='logout'),
 ]
