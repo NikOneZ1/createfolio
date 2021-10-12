f4d624a28ced72bc68ce56c3529f7ff333e0da69
Nikita(PC) <nikita.verba04@mail.ru>
2021-10-12 13:18:29 +0300

    Deleted all create, update and delete templates instead of them two templates were made create_update_template and delete_template

--- main/views.py
+++ main/views.py
@@ -41,7 +41,6 @@ def register(request):
 @login_required
 def create_portfolio(request):
     obj = Portfolio.objects.create(user=request.user)
-    print(obj.link)
     return redirect(change_portfolio, slug=obj.link)
 
 
@@ -54,7 +53,7 @@ def change_portfolio(request, slug):
     data = {
         'portfolio': Portfolio.objects.get(link=slug),
         'projects': Project.objects.filter(portfolio=Portfolio.objects.get(link=slug)),
-        'contacts': Contact.objects.filter(portfolio=Portfolio.objects.get(link=slug))
+        'contacts': Contact.objects.filter(portfolio=Portfolio.objects.get(link=slug)),
     }
     return render(request, 'main/change_portfolio.html', data)
 
@@ -62,7 +61,7 @@ def change_portfolio(request, slug):
 class UpdateChangeMe(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Portfolio
     fields = ['image', 'header', 'about_me', 'link']
-    template_name = 'main/change_about_me.html'
+    template_name = 'main/change_create_template.html'
 
     def test_func(self):
         obj = self.get_object()
@@ -76,11 +75,18 @@ class UpdateChangeMe(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         self.success_url = '/change_portfolio/' + form.instance.link + '/'
         return super().form_valid(form)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Change about me'
+        context['header'] = 'Change'
+        context['button_text'] = 'Save'
+        return context
+
 
 class UpdateProject(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Project
     fields = ['image', 'name', 'description', 'project_link']
-    template_name = 'main/change_project.html'
+    template_name = 'main/change_create_template.html'
 
     def test_func(self):
         obj = self.get_object()
@@ -92,12 +98,19 @@ class UpdateProject(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def form_valid(self, form):
         self.success_url = '/change_portfolio/' + form.instance.portfolio.link + '/'
         return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Change project'
+        context['header'] = 'Change project'
+        context['button_text'] = 'Save'
+        return context
 
 
 class UpdateContact(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Contact
     fields = ['logo', 'social_network', 'link']
-    template_name = 'main/change_contact.html'
+    template_name = 'main/change_create_template.html'
 
     def test_func(self):
         obj = self.get_object()
@@ -110,11 +123,18 @@ class UpdateContact(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         self.success_url = '/change_portfolio/' + form.instance.portfolio.link + '/'
         return super().form_valid(form)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Change contact'
+        context['header'] = 'Change contact'
+        context['button_text'] = 'Save'
+        return context
+
 
 class CreateProject(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Project
     fields = ['image', 'name', 'description', 'project_link']
-    template_name = 'main/create_project.html'
+    template_name = 'main/change_create_template.html'
 
     def test_func(self):
         portfolio = Portfolio.objects.get(link=self.kwargs['slug'])
@@ -127,12 +147,19 @@ class CreateProject(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         form.instance.portfolio = Portfolio.objects.get(link=self.kwargs['slug'])
         self.success_url = '/change_portfolio/' + self.kwargs['slug'] + '/'
         return super().form_valid(form)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Create project'
+        context['header'] = 'Create project'
+        context['button_text'] = 'Create'
+        return context
 
 
 class CreateContact(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Contact
     fields = ['logo', 'social_network', 'link']
-    template_name = 'main/create_contact.html'
+    template_name = 'main/change_create_template.html'
 
     def test_func(self):
         portfolio = Portfolio.objects.get(link=self.kwargs['slug'])
@@ -146,10 +173,17 @@ class CreateContact(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         self.success_url = '/change_portfolio/' + self.kwargs['slug'] + '/'
         return super().form_valid(form)
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Create contact'
+        context['header'] = 'Create contact'
+        context['button_text'] = 'Create'
+        return context
+
 
 class DeleteProject(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Project
-    template_name = 'main/delete_project.html'
+    template_name = 'main/delete_template.html'
 
     def test_func(self):
         project = self.get_object()
@@ -158,10 +192,18 @@ class DeleteProject(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return True
         return False
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        project = self.get_object()
+        context['title'] = 'Delete project'
+        context['header'] = 'Are you sure you want to delete the project?'
+        context['link_back'] = '/change_portfolio/' + project.portfolio.link + '/'
+        return context
+
 
 class DeleteContact(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Contact
-    template_name = 'main/delete_contact.html'
+    template_name = 'main/delete_template.html'
 
     def test_func(self):
         contact = self.get_object()
@@ -170,11 +212,26 @@ class DeleteContact(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
             return True
         return False
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        project = self.get_object()
+        context['title'] = 'Delete contact'
+        context['header'] = 'Are you sure you want to delete the contact?'
+        context['link_back'] = '/change_portfolio/' + project.portfolio.link + '/'
+        return context
+
 
 class DeletePortfolio(LoginRequiredMixin, DeleteView):
     model = Portfolio
-    template_name = 'main/delete_portfolio.html'
+    template_name = 'main/delete_template.html'
     success_url = '/profile/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Delete portfolio'
+        context['header'] = 'Are you sure you want to delete the portfolio?'
+        context['link_back'] = '/profile/'
+        return context
 
 
 @login_required
