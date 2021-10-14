ff0bfa3a41d4af36200d110472d87400e6e28525
Nikita(PC) <nikita.verba04@mail.ru>
2021-10-14 14:26:49 +0300

    Preparing a website for publication

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-$%&r5fteje_fcc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['createfolio.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
