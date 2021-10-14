5ddfcbd2606a114525b5571ada32dcd2e5116222
Nikita(PC) <nikita.verba04@mail.ru>
2021-10-14 13:16:33 +0300

    preparing a website for publication

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$%&r5fteje_fcc7-f%=$n*7ke7o6rv)jku9e8!!qw=9*i6z-pl'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-$%&r5fteje_fcc7-f%=$n*7ke7o6rv)jku9e8!!qw=9*i6z-pl')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
@@ -134,3 +135,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
