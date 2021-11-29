71555b902fb454e5725981f0f547d79a789ae264
Nikita(PC) <nikita.verba04@mail.ru>
2021-11-29 14:50:54 +0200

    Added password reset functionality

--- main/urls.py
+++ main/urls.py
@@ -6,11 +6,24 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('portfolio/<portfolio_name>', views.portfolio, name='portfolio'),
-    path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='main/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='main/logout.html'), name='logout'),
+    path('reset_password/',
+         auth_views.PasswordResetView.as_view(template_name='main/password_reset.html'),
+         name='reset_password'),
+    path('reset_password_sent/',
+         auth_views.PasswordResetDoneView.as_view(template_name='main/password_reset_done.html'),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(template_name='main/reset.html'),
+         name='password_reset_confirm'),
+    path('reset_password_complete/',
+         auth_views.PasswordResetCompleteView.as_view(template_name='main/reset_password_complete.html'),
+         name='password_reset_complete'),
+
+    path('portfolio/<portfolio_name>', views.portfolio, name='portfolio'),
+    path('', views.home, name='home'),
     path('create_portfolio/', views.create_portfolio, name='create_portfolio'),
     path('change_portfolio/<slug>/', views.change_portfolio, name='change_portfolio'),
     path('change_about_me/<int:pk>/', views.UpdateAboutMe.as_view(), name='change_about_me'),
@@ -22,6 +35,7 @@ urlpatterns = [
     path('delete_contact/<int:pk>/', views.DeleteContact.as_view(), name='delete_contact'),
     path('delete_portfolio/<int:pk>/', views.DeletePortfolio.as_view(), name='delete_portfolio'),
     path('profile/', views.profile, name='profile'),
+
     path('api/portfolio/<link>', views.PortfolioView.as_view(), name='api_portfolio'),
     path('api/user_portfolio', views.UserPortfolioListView.as_view(), name='api_user_portfolio'),
     path('api/create_portfolio', views.PortfolioCreateView.as_view(), name='api_create_portfolio'),
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -162,3 +162,12 @@ LOGIN_URL = 'login'
 # Heroku: Update database configuration from $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
